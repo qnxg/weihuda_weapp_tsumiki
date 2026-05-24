1309628cfd413cd42942d33997c885d3fafd67c7
Twisuki <Twisuki@outlook.com>
2026-05-24 22:16:56 +0800

    feat: 通用存储类

---
+++ src/utils/storage.ts
@@ -0,0 +1,72 @@
+import Taro from "@tarojs/taro"
+
+/**
+ * @description 通用存储类
+ * @template T - 存储数据的类型
+ * @param {string} key - 存储键
+ * @param {T} [value] - 可选的初始值, 如果提供则会立即存储
+ * @property {string} key - 存储键
+ * @property {() => Promise<T | null>} get - 获取存储数据
+ * @property {(value: T) => Promise<void>} set - 设置存储数据
+ * @property {() => Promise<void>} remove - 删除存储数据
+ */
+export class Storage<T> {
+  key: string
+
+  constructor(key: string, value?: T) {
+    this.key = key
+
+    if (value !== undefined) {
+      void this.set(value)
+    }
+  }
+
+  async get(): Promise<T | null> {
+    return new Promise<T | null>((resolve, _) => {
+      Taro.getStorage({
+        key: this.key,
+        success: res => resolve(res.data as T),
+        fail: (err) => {
+          console.error(`[STORAGE ERROR ${this.key}]`, err)
+          resolve(null)
+        },
+      }).catch((error) => {
+        console.error(`[STORAGE ERROR ${this.key}]`, error)
+        resolve(null)
+      })
+    })
+  }
+
+  async set(value: T): Promise<void> {
+    return new Promise<void>((resolve, _) => {
+      Taro.setStorage({
+        key: this.key,
+        data: value,
+        success: () => resolve(),
+        fail: (err) => {
+          console.error(`[STORAGE ERROR ${this.key}]`, err)
+          resolve()
+        },
+      }).catch((error) => {
+        console.error(`[STORAGE ERROR ${this.key}]`, error)
+        resolve()
+      })
+    })
+  }
+
+  async remove(): Promise<void> {
+    return new Promise<void>((resolve, _) => {
+      Taro.removeStorage({
+        key: this.key,
+        success: () => resolve(),
+        fail: (err) => {
+          console.error(`[STORAGE ERROR ${this.key}]`, err)
+          resolve()
+        },
+      }).catch((error) => {
+        console.error(`[STORAGE ERROR ${this.key}]`, error)
+        resolve()
+      })
+    })
+  }
+}
