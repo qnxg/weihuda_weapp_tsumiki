db669d7edea94cae71487459de1d3f3d95e533ae
Twisuki <Twisuki@outlook.com>
2026-06-03 13:17:54 +0800

    chore: 添加 css 等的 formater

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,11 @@
 import antfu from "@antfu/eslint-config"
 
 export default antfu({
+  formatters: {
+    css: true,
+    html: true,
+    markdown: true,
+  },
   stylistic: {
     indent: 2,
     quotes: "double",
