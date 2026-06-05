a20e70cae7895615d9957f5765a26d66eed0e761
Twisuki <Twisuki@outlook.com>
2026-06-05 22:45:46 +0800

    feat(课表页): 为课表选项添加动画

--- src/pages/table/components/options.tsx
+++ src/pages/table/components/options.tsx
@@ -8,6 +8,7 @@ import { useUser } from "@/hooks/user"
 import CloseIcon from "@/static/table/close.svg"
 import { logger } from "@/utils/logger"
 import { getPrevSemester, getSemesterFromName, getSemesterName } from "@/utils/semester"
+import "./index.scss"
 
 export function Options({
   enable,
@@ -32,14 +33,14 @@ export function Options({
 }>) {
   const { user } = useUser()
 
+  // 根据当前学期和用户信息生成学期选择范围, 最多包含 20 个学期(防止死循环)
   const getPickerSemesterRange = () => {
     if (!user)
       return [getSemesterName(semester)]
 
     // 倒序得到学期数组
     const semesters = [semester]
-    let times = 0
-    while (true) {
+    for (let times = 0; times <= 20; times++) {
       const prev = getPrevSemester(semesters.at(-1)!)
       semesters.push(prev)
 
@@ -47,9 +48,8 @@ export function Options({
         break
 
       times += 1
-      if (times > 20) {
+      if (times === 20) {
         logger.error(LABEL.page.table.options.SEMESTER_NOT_ACCESSIBLE, `semester: ${JSON.stringify(semester)}`)
-        break
       }
     }
     return semesters.map(s => getSemesterName(s))
@@ -59,25 +59,51 @@ export function Options({
   const pickerWeekRange: string[] = Array.from({ length: weeks }).map((_, i) => `${i + 1}`)
   const pickerRange = [pickerSemesterRange, pickerWeekRange]
 
+  // 用于学期 / 星期的 Picker 组件
   const [picker, setPicker] = useState([0, week - 1])
 
+  // 控制页面进入 / 移出动画
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleClose = () => {
+    setIsOpen(false)
+
+    setTimeout(() => {
+      onClose()
+    }, 200)
+  }
+
+  // 应用 picker 变化
   useEffect(() => {
     onSemesterChange(getSemesterFromName(pickerSemesterRange[picker[0]])!)
     onWeekChange(picker[1] + 1)
   }, [onSemesterChange, onWeekChange, picker, pickerSemesterRange])
 
+  // 初始挂载, 使用 setTimeout 宏任务防止在 transition 加载完成前 transform 变化
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsOpen(true)
+    }, 0)
+
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <View
       className="w-full h-full bg-shadow flex flex-col justify-end"
-      onClick={() => onClose()}
+      onClick={() => handleClose()}
     >
       <View
         className="bg flex flex-col p-xl gap"
+        style={{
+          transform: isOpen ? "translateY(0)" : "translateY(100%)",
+          transition: "transform 0.2s ease",
+        }}
         onClick={e => e.stopPropagation()}
       >
         <View className="flex items-center justify-between text-2xl text-bold">
           <View>课表选项</View>
-          <View onClick={() => onClose()}>
+          <View onClick={() => handleClose()}>
             <Icon
               style={{
                 width: "32rpx",
