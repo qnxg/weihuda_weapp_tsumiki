f19f0e8868ab5565f4d6154a05857aedfb2ef4b3
Twisuki <Twisuki@outlook.com>
2026-06-11 19:40:57 +0800

    feat(个人页): 样式微调

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -80,11 +80,11 @@ export default function Profile() {
           <View className="h-full flex flex-col gap">
             <Card>
               <View className="flex gap p">
-                <View className="size-xs rounded-full bg-primary flex center text-2xl text-reverse">
+                <View className="size-sm rounded-full bg-primary flex center text-2xl text-reverse">
                   {user ? user.name.at(0) : "U"}
                 </View>
                 <View className="flex-1 flex flex-col justify-center gap-xs">
-                  <View className="text-lg text-bold">{user ? user.name : "小程序用户"}</View>
+                  <View className="text-xl text-bold">{user ? user.name : "小程序用户"}</View>
                   <View className="text-toned">{user ? user.stu_id : "---"}</View>
                 </View>
               </View>
@@ -92,22 +92,22 @@ export default function Profile() {
 
             <Card>
               <View className="flex center gap">
-                <View className="w-2xl h-xl flex flex-col center">
+                <View className="w-2xl h-md flex flex-col center">
                   <View className="size-xs flex center text-2xl bold">{jifenData?.jifen ?? "--"}</View>
                   <View>当前积分</View>
                 </View>
 
-                <View className="h-xl bg-page" style={{ width: "6rpx" }} />
+                <View className="h-md bg-page" style={{ width: "6rpx" }} />
 
-                <View className="w-2xl h-xl flex flex-col center">
+                <View className="w-2xl h-md flex flex-col center">
                   <View className="size-xs flex center text-2xl bold">{jifenData?.combo ?? "--"}</View>
                   <View>连续签到</View>
                 </View>
 
-                <View className="h-xl bg-page" style={{ width: "6rpx" }} />
+                <View className="h-md bg-page" style={{ width: "6rpx" }} />
 
                 <View
-                  className="w-2xl h-xl flex flex-col center"
+                  className="w-2xl h-md flex flex-col center"
                   onClick={() => navigate("/pages/jifen/index")}
                 >
                   <View className="size-xs flex center">
