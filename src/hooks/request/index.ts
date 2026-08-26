d8a09b54a447804f618d78d6db1c9e2d90fc7acc
Twisuki <twisuki@outlook.com>
2026-08-26 14:07:24 +0800

    feat(query): 新增 mutation hook

--- src/hooks/request/index.ts
+++ src/hooks/request/index.ts
@@ -4,16 +4,5 @@
  * tanstack/react-query 在 Taro (小程序) 环境下存在兼容性问题, 因此仿照其 API, 简化实现一套等价 hook
  */
 
-export {
-  KEEP_PREVIOUS_DATA,
-  useQuery,
-} from "./query"
-
-export type {
-  FetchStatus,
-  KeepPreviousData,
-  QueryFunction,
-  QueryStatus,
-  UseQueryOptions,
-  UseQueryResult,
-} from "./query"
+export * from "./mutation"
+export * from "./query"
