796aecfd6767b191ef2f606e3942f89b750f1d3e
Twisuki <twisuki@outlook.com>
2026-08-27 17:00:54 +0800

    refactor(query): cached request 迁移

--- src/hooks/course.ts
+++ src/hooks/course.ts
@@ -4,28 +4,23 @@ import type { Semester } from "@/types/semester"
 import { useCallback, useMemo } from "react"
 import { api } from "@/apis"
 import { STORAGE } from "@/config/storage-key"
-import { useCachedRequest } from "@/hooks/cached-request"
-import { mockRequest } from "@/utils/mock-request"
+import { useCachedQuery } from "@/hooks/request"
 
-type CourseResult = ReturnType<typeof useCachedRequest<ClasstableItem[]>>
-type ExtraCourseResult = ReturnType<typeof useCachedRequest<ExtraCourseItem[]>>
+type CourseResult = ReturnType<typeof useCachedQuery<ClasstableItem[]>>
+type ExtraCourseResult = ReturnType<typeof useCachedQuery<ExtraCourseItem[]>>
 
 /**
  * @description 课表 Hook, 用于兼容 semester 未就绪时的缓存请求 Hook
  * @param {Semester | null} semester - 指定学期, 兼容 null 情况
  */
 export function useCourse(semester: Semester | null): CourseResult {
-  // 在 semester 未就绪时使用一个立即 resolve 的空请求函数, 以兼容 useCachedRequest
-  // 为使类型定义更为简单, 使用 mockRequest 作为上述 "立即 resolve 的空请求函数"
-  const fn = useCallback(() => semester
-    ? api.classtable.get(semester)
-    : mockRequest([], { delay: 0 }), [semester])
+  const fn = useCallback(() => api.classtable.get(semester!), [semester])
 
   const key = useMemo(() => semester
     ? `${STORAGE.page.table.course.prefix}_${semester.xn}_${semester.xq}`
     : `${STORAGE.page.table.course.prefix}_${STORAGE.page.table.course.placeholder}`, [semester])
 
-  return useCachedRequest(fn, key, { enabled: !!semester })
+  return useCachedQuery(fn, [semester], key, { enabled: !!semester })
 }
 
 /**
@@ -33,15 +28,11 @@ export function useCourse(semester: Semester | null): CourseResult {
  * @param {Semester | null} semester - 指定学期, 兼容 null 情况
  */
 export function useExtraCourse(semester: Semester | null): ExtraCourseResult {
-  // 在 semester 未就绪时使用一个立即 resolve 的空请求函数, 以兼容 useCachedRequest
-  // 为使类型定义更为简单, 使用 mockRequest 作为上述 "立即 resolve 的空请求函数"
-  const fn = useCallback(() => semester
-    ? api.course.getExtra(semester)
-    : mockRequest([], { delay: 0 }), [semester])
+  const fn = useCallback(() => api.course.getExtra(semester!), [semester])
 
   const key = useMemo(() => semester
     ? `${STORAGE.page.table.extra.prefix}_${semester.xn}_${semester.xq}`
     : `${STORAGE.page.table.extra.prefix}_${STORAGE.page.table.course.placeholder}`, [semester])
 
-  return useCachedRequest(fn, key, { enabled: !!semester })
+  return useCachedQuery(fn, [semester], key, { enabled: !!semester })
 }
--- src/hooks/grade.ts
+++ src/hooks/grade.ts
@@ -1,18 +1,14 @@
-import type { GradeItem } from "@/apis/models/grade"
 import type { Semester } from "@/types/semester"
 import { useCallback } from "react"
 import { api } from "@/apis"
-import { useRequest } from "@/hooks/request"
-import { mockRequest } from "@/utils/mock-request"
+import { useQuery } from "@/hooks/request"
 
 /**
  * @description 成绩 Hook, 用于兼容 semester 未就绪时的请求
  * @param {Semester | null} semester - 指定学期, 兼容 null 情况
  */
 export function useGrade(semester: Semester | null) {
-  const fn = useCallback(() => semester
-    ? api.grade.get(semester)
-    : mockRequest<GradeItem[]>([], { delay: 0 }), [semester])
+  const fn = useCallback(() => api.grade.get(semester!), [semester])
 
-  return useRequest(fn, [semester])
+  return useQuery(fn, [semester], { enabled: !!semester })
 }
